895aea4ec9d5da0ca3b185a15a87ca361a19c1c7
Programmer NBA <137274609+programmer-nba@users.noreply.github.com>
2024-02-28 19:01:35 +0700

    up

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template >
-  <div v-if="this.$store.getters.logedIn === true" class="" >
-    <NavberPartner v-if="this.$store.getters.position === 'partner' " />
-    <router-view  /> 
+  <div v-if="this.$store.getters.logedIn === true" class="">
+    <NavberPartner v-if="this.$store.getters.position === 'partner'" />
+    <router-view />
   </div>
 
   <div v-else>
@@ -10,7 +10,7 @@
 
   <Toast />
   <ConfirmDialog>
-  
+
   </ConfirmDialog>
   <!-- dialog loading-->
   <Dialog class="border-0 shadow-none" :draggable="false" v-model:visible="this.$store.getters.loading" :closable="false"
@@ -42,44 +42,44 @@ export default {
       console.log('Decoded Token:', decode);
 
       if (decode) {
-      await axios
-        .get(`${process.env.VUE_APP_API}/partner/me/`, {
-          headers: {
-            "token": `${localStorage.getItem("token")}`,
-          },
-        })
-        .then(async (res) => {
-          const decode = jwtDecode(localStorage.getItem("token"));
-          const data_login = {
-            logedIn: true,
-            _id:res.data.data._id,
-            username:res.data.data.username,
-            firstname:res.data.data.firstname,
-            lastname:res.data.data.lastname,
-            nickname:res.data.data.nickname,
-            position:res.data.data.position,
-            status_appover:decode.status_appover,
+        await axios
+          .get(`${process.env.VUE_APP_API}/partner/me/`, {
+            headers: {
+              "token": `${localStorage.getItem("token")}`,
+            },
+          })
+          .then(async (res) => {
+            const decode = jwtDecode(localStorage.getItem("token"));
+            const data_login = {
+              logedIn: true,
+              _id: res.data.data._id,
+              username: res.data.data.username,
+              firstname: res.data.data.firstname,
+              lastname: res.data.data.lastname,
+              nickname: res.data.data.nickname,
+              position: res.data.data.position,
+              status_appover: decode.status_appover,
 
-          };
-          console.log('position Token:', decode.position);
-          console.log('status จากโทเคน:', decode.status_appover);
+            };
+            console.log('position Token:', decode.position);
+            console.log('status จากโทเคน:', decode.status_appover);
 
-          console.log(res.data.data)
-          this.$store.commit("setLogin", data_login);
-          console.log(this.$store.getters.position);
-  
+            console.log(res.data.data)
+            this.$store.commit("setLogin", data_login);
+            console.log(this.$store.getters.position);
 
-          
-        })
-        .catch(() => {
-          localStorage.clear();
-          this.$router.push("/login");
-        });
+
+
+          })
+          .catch(() => {
+            localStorage.clear();
+            this.$router.push("/login");
+          });
       } else {
-      // ถ้า token ไม่มีค่าหรือ decode ไม่ได้ ให้ทำการ clear localStorage และ redirect ไปยังหน้า login
-      localStorage.clear();
-      this.$router.push("/login");
-    }
+        // ถ้า token ไม่มีค่าหรือ decode ไม่ได้ ให้ทำการ clear localStorage และ redirect ไปยังหน้า login
+        localStorage.clear();
+        this.$router.push("/login");
+      }
     } else {
       localStorage.clear();
       this.$router.push("/login");
@@ -89,42 +89,51 @@ export default {
 </script>
 
 <style>
-.p-menuitem-icon{
-  color:#0e4f26;
-  
+.p-menuitem-icon {
+  color: #0e4f26;
+
 }
-.p-menubar .p-menubar-root-list > .p-menuitem:not(.p-highlight):not(.p-disabled) > .p-menuitem-content:hover .p-menuitem-link .p-menuitem-text{
-  color:#fff;
+
+.p-menubar .p-menubar-root-list>.p-menuitem:not(.p-highlight):not(.p-disabled)>.p-menuitem-content:hover .p-menuitem-link .p-menuitem-text {
+  color: #fff;
 }
-.p-menubar .p-menubar-root-list > .p-menuitem > .p-menuitem-content .p-menuitem-link .p-menuitem-text{
-  color:#0e4f26;
+
+.p-menubar .p-menubar-root-list>.p-menuitem>.p-menuitem-content .p-menuitem-link .p-menuitem-text {
+  color: #0e4f26;
 }
-.p-menuitem-content:hover{
+
+.p-menuitem-content:hover {
   background: #354390;
 }
-.p-menuitem-content{
+
+.p-menuitem-content {
   background: none;
 }
+
 #app {
   font-family: 'Kanit';
   src: url('@/assets/Kanit/Kanit-Regular.ttf') format('truetype');
 }
-.font{
+
+.font {
   font-family: 'Kanit' !important;
   src: url('@/assets/Kanit/Kanit-Regular.ttf') format('truetype') !important;
 }
-.p-button{
+
+.p-button {
 
   background-color: #1da750 !important;
-  border:none;
+  border: none;
 }
-.p-component{
-   font-family: 'Kanit';
+
+.p-component {
+  font-family: 'Kanit';
   src: url('@/assets/Kanit/Kanit-Regular.ttf') format('truetype');
   font-weight: normal;
-  font-style: normal; 
+  font-style: normal;
 }
-.p-inputtext{
+
+.p-inputtext {
   font-family: 'Kanit' !important;
   src: url('@/assets/Kanit/Kanit-Regular.ttf') format('truetype') !important;
 }
